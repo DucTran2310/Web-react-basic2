0c68f8ab2baf7df5b962c9050f159a41e355c837
DucTran2310 <18520021@gm.uit.edu.vn>
2021-09-09 21:22:22 +0700

    Update everything

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import Navbar from "./components/Navbar/Navbar";
+import {BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
+import Home from "pages/HomePage/Home";
+import Services from "pages/Services/Services";
+import Footer from "components/Footer/Footer";
+import Product from "pages/Product/Product";
+import SignUp from "pages/SignUp/SignUp";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Navbar />
+            <Switch>
+                <Route path="/" exact component={Home} />
+                <Route path="/services"  component={Services} />
+                <Route path="/products"  component={Product} />
+                <Route path="/sign-up"  component={SignUp} />
+            </Switch>
+            <Footer />
+        </Router>
+    );
 }
 
 export default App;
